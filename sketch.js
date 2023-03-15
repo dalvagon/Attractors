905f6fbd6ab86e429ac45587fbe24db87924e8c2
Leahu Vlad Iulius <leahu.vlad.iulius@gmail.com>
2023-03-15 15:52:52 +0200

    Create gifs

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
 let points = [];
 let x = 0.1;
-let y = 1;
-let z = 1;
-let hu = 0;
-let dt = 0.02;
+let y = 0;
+let z = 0;
+let dt = 0.05;
 
 function setup() {
   createCanvas(1519, 864, WEBGL);
+  stroke(random(255), random(255), random(255));
 }
 
 function LorenzAttractor() {
@@ -44,14 +44,9 @@ function AizawaAttractor() {
   const e = 0.25;
   const f = 0.1;
   dx = ((z - b) * x - d * y) * dt;
-  dy = (d * x - (z - b) * y) * dt;
-  dz =
-    (c +
-      a * z -
-      pow(z, 3) / 3 -
-      (pow(x, 2) + pow(y, 2)) * (1 + e * z) +
-      f * z * pow(x, 3)) *
-    dt;
+  dy = (d * x + (z - b) * y) * dt;
+  dz = (c + a * z - z ** 3 / 3 - (x ** 2 + y ** 2) * (1 + e * z)) * dt;
+
   x += dx;
   y += dy;
   z += dz;
@@ -75,8 +70,42 @@ function RosslerAttractor() {
   points.push(createVector(x, y, z));
 }
 
+function ChenLeeAttractor() {
+  const a = 5;
+  const b = -10;
+  const d = -0.38;
+
+  dx = (a * x - y * z) * dt;
+  dy = (b * y + x * z) * dt;
+  dz = (d * z + (x * y) / 3) * dt;
+
+  x += dx;
+  y += dy;
+  z += dz;
+
+  points.push(createVector(x, y, z));
+}
+
+function SprottBAttractor() {
+  const a = 0.4;
+  const b = 1.2;
+  const c = 1;
+
+  dx = a * y * z * dt;
+  dy = (x - b * y) * dt;
+  dz = (c - x * y) * dt;
+
+  x += dx;
+  y += dy;
+  z += dz;
+
+  points.push(createVector(x, y, z));
+}
+
 function draw() {
-  RosslerAttractor();
+  frameRate(100);
+
+  SprottBAttractor();
 
   background(0);
   translate(0, 0, -80);
@@ -85,7 +114,7 @@ function draw() {
   camera(
     camX,
     camY,
-    height / (2.0 * tan((PI * 30.0) / 180.0)),
+    height / (4.0 * tan((PI * 30.0) / 180.0)),
     0,
     0,
     0,
@@ -100,14 +129,11 @@ function draw() {
 
 function drawAttractor() {
   noFill();
-  strokeWeight(1.5);
-  stroke(255);
+  strokeWeight(0.7);
 
   beginShape();
   for (let p of points) {
-    stroke(128, 255, 255);
     vertex(p.x, p.y, p.z);
   }
-  hu = (hu + 1) % 255;
   endShape();
 }
